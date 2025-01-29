109ae7c75f4199416ae70f481b2728ca6e34e691
Daniel Levi <daniellevi000@gmail.com>
2025-01-29 20:29:42 -0300

    feat: create routes pages

---
+++ app/Views/where_we_are.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CIGBurguer</title>
+  <link rel="stylesheet" href="<?= base_url('assets/bootstrap/bootstrap.min.css') ?>" >
+</head>
+<body>
+  <h1>Onde estamos</h1>
+
+  <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
+</body>
+</html>
